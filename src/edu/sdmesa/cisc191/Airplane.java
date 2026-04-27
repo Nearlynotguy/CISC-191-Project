f9b1862cb6f741835b42935f4ea76eaabcec531a
Nearlynotguy <nvilbran@purdue.edu>
2026-04-27 13:46:14 -0700

    Added Airplane Class.

---
+++ src/edu/sdmesa/cisc191/Airplane.java
@@ -0,0 +1,42 @@
+/**
+ * Lead Author(s):
+ * 
+ * @author Nicholas Vilbrandt
+ * @author
+ *         <<add additional lead authors here, with a full first and last name>>
+ * 
+ *         Other contributors:
+ *         <<add additional contributors (mentors, tutors, friends) here, with
+ *         contact information>>
+ * 
+ *         References:
+ *         Morelli, R., & Walde, R. (2016). Java, Java, Java: Object-Oriented
+ *         Problem Solving.
+ *         Retrieved from
+ *         https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+ * 
+ *         <<add more references here>>
+ * 
+ *         Version/date: 2026-04-27
+ */
+
+package edu.sdmesa.cisc191;
+
+/**
+ * Purpose: The responsibility of Airplane is ...
+ *
+ * Airplane is-a ...
+ * Airplane is ...
+ */
+public class Airplane
+{
+
+	/**
+	 * Purpose: 
+	 */
+	public Airplane()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
